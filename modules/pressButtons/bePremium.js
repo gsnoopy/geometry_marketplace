6bbd0722d8cf948da2e443b60ff8adab03e78273
Gustavo Juliano Borges <awkzdev@gmail.com>
2024-01-26 23:20:52 -0300

    Create BePremium

---
+++ modules/pressButtons/bePremium.js
@@ -0,0 +1,6 @@
+
+
+// getUserById para verificar se tem saldo suficiente
+// se tiver saldo suficiente mostrar a mensagem com dois botao
+// botão confirmPremium para retirar saldo e dar cargo
+// botão cancel para deletar mensagem
