fb93652e523555e8899abd56070ef4a477012927
Gustavo Juliano Borges <awkzdev@gmail.com>
2024-02-03 23:48:43 -0300

    Study and create verify Ads e Premiums

---
+++ scripts/verifyAds.js
@@ -0,0 +1,3 @@
+Verificar transactions_anuncios campo data_hora
+
+se data_hora for >= 24h, adicionar saldo_retido no saldo do seller_id
---
+++ scripts/verifyPremiums.js
@@ -0,0 +1,2 @@
+
+verificar tabela premium, se data_registro >= 30 dias, remover role Premium do user_id
