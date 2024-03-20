a0468b00243e93b3a2c7c3f1b0656ca7b9678155
Gustavo Juliano Borges <awkzdev@gmail.com>
2024-03-20 14:15:55 -0300

    security, ticket texts and Icons

--- comandos/admins/securityEmbed.js
+++ comandos/admins/securityEmbed.js
@@ -15,8 +15,9 @@ module.exports = {
 
       let embed = new Discord.EmbedBuilder()
         .setColor(0x020202)
+        .setTitle("Um ambiente seguro")
         .setImage("https://media.discordapp.net/attachments/1204152785831206964/1209913846295437322/security.png?ex=65e8a714&is=65d63214&hm=5c01099a760aa2b296517ccd89310ae46ad3b56ca3e4808ef64359bc8be2dca9&=&format=webp&quality=lossless&width=1626&height=574")
-        .setDescription("Security Embed")
+        .setDescription("<:secIcon4:1210607761277194371> Identificação única\n<:secIcon3:1210607759947603988> Termos e privacidade\n<:secIcon2:1210607758055972997> Bloqueio contra Scammers\n<:secIcon6:1216976179479973888> Armazenamento de dados\n<:secIcon1:1210607755572936824> Cloud e proteção de dados\n<:secIcon5:1210607762707447889> Logs e transcripts\n\n Clique nos botões abaixo e acesse os detalhes das medidas de segurança ou os termos de uso e privacidade.")
 
         const button = new Discord.ActionRowBuilder().addComponents(
           new Discord.ButtonBuilder()
--- comandos/admins/ticketEmbed.js
+++ comandos/admins/ticketEmbed.js
@@ -15,14 +15,15 @@ module.exports = {
 
       let embed = new Discord.EmbedBuilder()
         .setColor(0x020202)
+        .setTitle("Central de atendimento ")
         .setImage("https://media.discordapp.net/attachments/1204152785831206964/1209760048750796850/support.png?ex=65e817d7&is=65d5a2d7&hm=716745205417e06b5a5ae5761befe53ac031c810599f7226a757a1921c4f90bc&=&format=webp&quality=lossless&width=1626&height=574")
-        .setDescription("Support Embed")
+        .setDescription("<:questionIcon:1220052561592320132> Possui uma dúvida ou um problema? clique no botão abaixo e você será colocado em atendimento privado com alguém da equipe Geometry Marketplace.")
         
       const button = new Discord.ActionRowBuilder().addComponents(
           new Discord.ButtonBuilder()
             .setCustomId("ticket")
-            .setLabel("Abrir suporte")
-            .setEmoji("<:signUp:1199292028165574767>") 
+            .setLabel("Abrir ticket")
+            .setEmoji("<:ticketIcon:1220052559935569924>") 
             .setStyle(Discord.ButtonStyle.Primary),
           new Discord.ButtonBuilder()
             .setLabel('Termos de uso')
